87c1028f0170ef51f5768f4344df2a3d6b1218e1
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-10-04 19:57:24 -0700

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,34 +1,54 @@
 #!/usr/bin/python3
 """
-Fabric script that distributes an archive to your web servers
+Fabric script for deploying an archive to web servers.
 """
-
-from datetime import datetime
-from fabric.api import *
+from fabric.decorators import dws
+from fabric.api import env, put, run
+from os.path import exists
+from os import remove
 import os
 
-env.hosts = ["18.204.14.176", "	54.226.7.139"]
-env.user = "ubuntu"
+# Update these with your actual server IP addresses and SSH key
+env.hosts = ['18.204.14.176', '54.226.7.139']
+env.user = 'ubuntu'
 
-def do_pack():
-    """
-        return the archive path if archive has generated correctly.
-    """
-
-    local("mkdir -p versions")
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    archived_f_path = "versions/web_static_{}.tgz".format(date)
-    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
-
-    if t_gzip_archive.succeeded:
-        return archived_f_path
-    else:
-        return None
-
-
+@dws
 def do_deploy(archive_path):
-    """
-        Distribute archive.
-    """
-    if os.path.exists(archive_path):
-        archived_file = archive_path[9:]
+    """Fabric script that distributes an archive to web servers"""
+    try:
+        with_ext = archive_path.split("/")[-1]
+        without_ext = archive_path.split("/")[-1].split(".")[0]
+        put(archive_path, "/tmp")
+        run("mkdir -p /data/web_static/releases/" + without_ext)
+        run(
+            "tar -xzf /tmp/"
+            + with_ext + " -C /data/web_static/releases/"
+            + without_ext
+        )
+        run("rm /tmp/" + with_ext)
+        run(
+            "mv /data/web_static/releases/"
+            + without_ext
+            + "/web_static/* /data/web_static/releases/"
+            + without_ext
+        )
+        run("rm -rf /data/web_static/releases/"
+            + without_ext + "/web_static")
+        run("rm -rf /data/web_static/current")
+        run(
+            "ln -s /data/web_static/releases/"
+            + without_ext
+            + "/ /data/web_static/current"
+        )
+        return True
+    except Exception:
+        return False
+
+
+@dws
+def do_pack():
+    """generates a .tgz archive from web_static"""
+    local(
+        "mkdir versions ; tar -cvzf \
+versions/web_static_$(date +%Y%m%d%H%M%S).tgz web_static/"
+    )
