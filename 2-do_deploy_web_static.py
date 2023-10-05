fcb98ea06976fd7177c702983543e4435069aa56
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-10-05 09:59:26 -0700

    do deploy web static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,38 +1,30 @@
 #!/usr/bin/python3
 """Script that does the deployment"""
-import os
-from fabric.api import run, put, env
-
+from fabric.api import *
+from datetime import datetime
+import os.path
 
 env.hosts = ['18.204.14.176', '54.226.7.139']
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to your web servers"""
+    """transfer files to web server"""
 
-    mkdir_cmd = "mkdir -p /data/web_static/releases/"
-    rm_cmd = "rm -rf /data/web_static/releases/"
-    deployed_success = "New Version Deployed!"
-    if os.path.exists(archive_path):
-        try:
-            put(archive_path, "/tmp/")
-            filename = archive_path.split('/', 1)
-            no_ext = filename[1].split('.', 1)
-            file_name = no_ext[0]
-            run(mkdir_cmd + file_name + "/")
-            run("tar -zxf /tmp/" + filename[1] +
-                " -C /data/web_static/releases/" +
-                file_name + "/")
-            run("rm /tmp/" + filename[1])
-            run("mv /data/web_static/releases/" + file_name +
-                "/web_static/* /data/web_static/releases/" + file_name + "/")
-            run(rm_cmd + file_name + "/web_static")
-            run("rm -rf /data/web_static/current")
-            run("ln -s /data/web_static/releases/" + file_name +
-                "/ /data/web_static/current")
-            print("{}".format(deployed_success))
-            return True
-        except:
-            return False
+    if os.path.isfile(archive_path):
+        pre_path = archive_path.split("/")[1]
+        put(archive_path, "/tmp/")
+        path_l = "/tmp/" + pre_path
+        path_r = "/data/web_static/releases/" + pre_path.split(".")[0]
+        sudo("mkdir -p {:s}".format(path_r))
+        sudo("tar -xzf {:s} -C {:s}".format(path_l, path_r))
+        sudo("rm {:s}".format(path_l))
+        path_m = path_r + "/web_static/*"
+        path_d = path_r + "/web_static/"
+        sudo("mv {:s} {:s}".format(path_m, path_r))
+        sudo("rm -rf {:s}".format(path_d))
+        sudo("rm -rf /data/web_static/current")
+        sudo("ln -s {:s} /data/web_static/current".format(path_r))
+        print("New version deployed!")
+        return True
     else:
         return False
