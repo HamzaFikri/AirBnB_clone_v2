34389a1110d7a5588d421220c2272951f5204821
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-10-07 18:00:03 -0700

    do deploy web static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -26,7 +26,7 @@ def do_deploy(archive_path):
     """ method doc
         fab -f 2-do_deploy_web_static.py do_deploy:
         archive_path=versions/web_static_20231004201306.tgz
-        -i ~/.ssh/id_rsa -u ubuntu
+        -i ~/.ssh/school -u ubuntu
     """
     try:
         if not os.path.exists(archive_path):
